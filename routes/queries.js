a1c7276f19c258278220feb70f85bbeaf83d27bf
Asdisarson <egill@islandsvefir.is>
2022-07-23 13:12:19 +0000

    f

--- routes/queries.js
+++ routes/queries.js
@@ -16,36 +16,40 @@ router.get('/', function(req, res, next) {
     };
     if(req.query) {
         options.url = options.url + '?';
+
+        if (req.query.propertyId) {
+            options.url = options.url + '&propertyId=' + req.query.propertyId;
+        }
+        if (req.query.numberOfExtraBeds) {
+            options.url = options.url + '&numberOfExtraBeds=' + req.query.numberOfExtraBeds;
+        }
+        if (req.query.numberOfRooms) {
+            options.url = options.url + '&numberOfRooms=' + req.query.numberOfRooms;
+        }
+        if (req.query.roomIds) {
+            options.url = options.url + '&roomIds=' + req.query.roomIds;
+        }
+        if (req.query.end) {
+            options.url = options.url + '&end=' + req.query.end;
+        }
+        if (req.query.start) {
+            options.url = options.url + '&start=' + req.query.start;
+        }
+        if (req.query.numberOfPeople) {
+            options.url = options.url + '&numberOfPeople=' + req.query.numberOfPeople;
+        }
+        if (req.query.numberOfExtraBeds) {
+            options.url = options.url + '&numberOfExtraBeds=' + req.query.numberOfExtraBeds;
+        }
+        request(options, function (error, response) {
+            if (error) throw new Error(error)
+            console.log(response.body);
+            res.send(response.body);
+        });
     }
-    if (req.query.propertyId) {
-        options.url = options.url + '&propertyId='+req.query.propertyId;
+    else {
+        res.sendStatus(503)
     }
-    if (req.query.numberOfExtraBeds) {
-        options.url = options.url + '&numberOfExtraBeds=' + req.query.numberOfExtraBeds;
-    }
-    if (req.query.numberOfRooms) {
-        options.url = options.url + '&numberOfRooms='+req.query.numberOfRooms;
-    }
-    if (req.query.roomIds) {
-        options.url = options.url + '&roomIds='+req.query.roomIds;
-    }
-    if (req.query.end) {
-        options.url = options.url + '&end='+req.query.end;
-    }
-    if (req.query.start) {
-        options.url = options.url + '&start='+req.query.start;
-    }
-    if (req.query.numberOfPeople) {
-        options.url = options.url + '&numberOfPeople='+req.query.numberOfPeople;
-    }
-    if (req.query.numberOfExtraBeds) {
-        options.url = options.url + '&numberOfExtraBeds='+req.query.numberOfExtraBeds;
-    }
-    request(options, function (error, response) {
-        if (error) throw new Error(error)
-        console.log(response.body);
-        res.send(response.body);
-    });
 });
 
 module.exports = router;
