7dd1512d2c618b409a08426d65a0b08c9d4aa63e
Asdisarson <egill@islandsvefir.is>
2022-07-29 22:03:13 +0000

    changing json response

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var queriesRouter = require('./routes/queries');
 var roomsRouter = require('./routes/rooms');
 var cartRouter = require('./routes/cart');
 const JSONdb = require("simple-json-db");
+const request = require("request");
+const options = require("./routes/auth");
 
 
 app.use(logger('dev'));
@@ -18,37 +20,39 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
     var request = require('request')
-  var db = new JSONdb('./db.json');
-        var options = require('./routes/auth.js')
-         return request(options, function (error, response) {
-            if (error) throw new Error(error)
-             var json = JSON.parse(response.body);
-            db.JSON(json);
-            db.sync();
-            return next();
-        });
-        // -----------------------------------------------------------------------
-    // authentication middleware
-/*
-    const auth = {login: 'yourlogin', password: 'yourpassword'} // change this
-
-    // parse login and password from headers
-    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-
-    // Verify login and password are set and correct
-    if (login && password && login === auth.login && password === auth.password) {
-        // Access granted...
-        return next()
-    }
-
-    // Access denied...
-    res.set('WWW-Authenticate', 'Basic realm="401"') // change this
-    res.status(401).send('Authentication required.') // custom message
+    var db = new JSONdb('./db.json');
+    var options = require('./routes/auth.js')
+    return request(options, function (error, response) {
+        if (error) throw new Error(error)
+        var json = JSON.parse(response.body);
+        db.JSON(json);
+        db.sync();
+        return next();
+    });
 
     // -----------------------------------------------------------------------
-*/
+    // authentication middleware
+    /*
+        const auth = {login: 'yourlogin', password: 'yourpassword'} // change this
+
+        // parse login and password from headers
+        const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+        const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+        // Verify login and password are set and correct
+        if (login && password && login === auth.login && password === auth.password) {
+            // Access granted...
+            return next()
+        }
+
+        // Access denied...
+        res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+        res.status(401).send('Authentication required.') // custom message
+
+        // -----------------------------------------------------------------------
+    */
 })
+
 app.use('/', indexRouter);
 app.use('/properties', propertiesRouter);
 app.use('/queries', queriesRouter);
--- routes/properties.js
+++ routes/properties.js
@@ -1,29 +1,65 @@
 var express = require('express');
 var router = express.Router();
-const JSONdb = require("simple-json-db");
 const request = require("request");
+const request1 = require("request");
+const request2 = require("request");
+
 /* GET users listing. */
 router.get('', function(req, res, next) {
-        var db = new JSONdb('./db.json');
-        var token = db.JSON();
-        var options = {}
-    if(req.query.id) {
-         options = {
-            'method': 'GET',
-            'url': 'https://stage-api.travia.is/api/v1/properties/'+req.query.id+'/details/cooperating',
-            'headers': {
-                'Authorization': 'Bearer ' + token.access_token
-            }
-        };
+    var JSONdb = require("simple-json-db");
+
+    var db = new JSONdb('./db.json');
+    var token = db.JSON();
+    var options = {
+        'method': 'GET',
+        'url': '',
+        'headers': {
+            'Authorization': 'Bearer ' + token.access_token
+        }
+    };
+    if (req.query.id) {
+        options.url = 'https://stage-api.travia.is/api/v1/properties/' + req.query.id + '/details/cooperating'
+    request(options, function (error, response) {
+        if (error) throw new Error(error)
+        console.log(response.body);
+        var array = {
+            result: []
+        }
+        if (req.query.id) {
+            array.result.push(
+                JSON.parse(response.body))
+        } else {
+            array.result =
+                JSON.parse(response.body)
+
+        }
+
+
+        res.send(array);
+    });
     }
     else if(req.query.room){
-        options = {
-            'method': 'GET',
-            'url': 'https://stage-api.travia.is/api/v1/properties/' + req.query.room + '/rooms',
-            'headers': {
-                'Authorization': 'Bearer ' + token.access_token
+        options.url =  'https://stage-api.travia.is/api/v1/properties/' + req.query.room + '/rooms'
+
+        request(options, function (error, response) {
+            if (error) throw new Error(error)
+            console.log(response.body);
+            var array = {
+                result : []}
+            if(req.query.id){
+                array.result.push(
+                    JSON.parse(response.body))
             }
-        }
+            else {
+                array.result =
+                    JSON.parse(response.body)
+
+            }
+
+
+            res.send(array);
+        });
+        return
     }
     else{
         options = {
@@ -32,25 +68,69 @@ router.get('', function(req, res, next) {
             'headers': {
                 'Authorization': 'Bearer ' + token.access_token
             }
-        }   }
-        ;
-          request(options, function (error, response) {
+        }
+        request(options, function (error, response) {
             if (error) throw new Error(error)
-              console.log(response.body);
-              var array = {
-                  result : []}
-              if(req.query.id){
-                  array.result.push(
-                      JSON.parse(response.body))
-              }
-              else {
-              array.result =
-                  JSON.parse(response.body)
+            console.log(response.body);
+            var array = {
+                result : []}
 
-              }
+                array.result =
+                    JSON.parse(response.body)
 
+            var options1 = {
+                'method': 'GET',
+                'headers': {
+                    'Authorization': 'Bearer ' + token.access_token
+                },
+                'url':  ''
+            }
+            var options2 = {
+                'method': 'GET',
+                'headers': {
+                    'Authorization': 'Bearer ' + token.access_token
+                },
+                'url':  ''
+            }
+            var array1 = {
+                result : []}
+            var json = new JSONdb('./json.json');
+            json.JSON(array1);
+            json.sync();
+            for (let i = 0; i < array.result.length; i++) {
 
-              res.send(array);
+                    console.log(array.result[i].id)
+                    options1.url ='https://stage-api.travia.is/api/v1/properties/'+array.result[i].id+'/details/cooperating'
+                    request1(options1, function (error1, response1) {
+                        if (error) throw new Error(error)
+
+                        options2.url =  'https://stage-api.travia.is/api/v1/properties/' + array.result[i].id + '/rooms'
+                        request2(options2, function (error2, response2) {
+                            if (error2) throw new Error(error2)
+                            var item2 =json.JSON();
+                            console.log(item2)
+                            item2.result.push({
+                                hotel:    JSON.parse(response1.body),
+                                rooms:JSON.parse(response2.body)}
+                            )
+                            json.JSON(item2)
+                            json.sync();
+                        });
+                    });
+
+            }
+            res.send(json.JSON());
         });
+    }
+        ;
+
+});
+router.get('/getall', function(req, res, next) {
+    var JSONdb = require("simple-json-db");
+
+    var db = new JSONdb('./json.json');
+    db.sync();
+ res.send(db.JSON())
+
 });
 module.exports = router;
