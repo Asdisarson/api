94557127ea8510916aa1f92c69a9fc42d7b4ed29
Asdisarson <egill@islandsvefir.is>
2022-08-01 00:26:41 +0000

    changing json response

--- routes/properties.js
+++ routes/properties.js
@@ -5,21 +5,47 @@ const JSONdb = require("simple-json-db");
 const {save} = require("./request");
 
 /* GET users listing. */
+router.get('', function(req, res, next) {
+    var db = new JSONdb('./db.json');
+    var cached = db.JSON();
+    if(cached.cache)  {
+        next();
+    }
+    else {
+        save();
+        next()
+    }
+});
+router.get('', function (req, res, next){
+    if(req.query.id)
+    {var db = new JSONdb('./data.json');
+        var data = db.JSON();
+        var result = data.data.find(obj => {
+            return obj.id.toString() === req.query.id;
+        })
+        res.send({
+            data:result
+        });}
+
+   else{
+       next();
+   }
+})
+router.get('', function (req, res, next){
+    var data = new JSONdb('./data.json');
+    console.log(data.JSON());
+    res.send(data.JSON());
+})
+
 router.get('/', function(req, res, next) {
     var db = new JSONdb('./db.json');
     var cached = db.JSON();
     if(cached.cache)  {
-         next();
+        next();
     }
     else {
-        var is =save();
-        if(is) {
-            next()
-
-        }
-        else {
-            res.send({})
-        }
+        save();
+        next()
     }
 });
 router.get('/', function (req, res, next){
