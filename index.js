1650a042a61a3c5bf0e3308a10718a6f8196be3e
Asdisarson <59482301+Asdisarson@users.noreply.github.com>
2022-08-08 17:44:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var propertiesRouter = require('./routes/properties');
 var queriesRouter = require('./routes/queries');
+var queriesRouter2 = require('./routes/queries2');
+
 var roomsRouter = require('./routes/rooms');
 var cartRouter = require('./routes/cart');
 const JSONdb = require("simple-json-db");
@@ -96,6 +98,7 @@ const JSONdb = require("simple-json-db");
 app.use('/', indexRouter);
 app.use('/properties', propertiesRouter);
 app.use('/search', queriesRouter);
+app.use('/search2', queriesRouter2);
 app.use('/rooms', roomsRouter);
 app.use('/cart', cartRouter);
 app
