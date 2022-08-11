cb87347d8665bbf2e4c27a28727a7cc56980f176
Asdisarson <egill@islandsvefir.is>
2022-08-11 13:51:59 +0000

    m

--- routes/cart.js
+++ routes/cart.js
@@ -23,18 +23,18 @@ router.post('/', function(req, res, next) {
     options.body = {
 
         "bookingRooms": [
-        {
-            "endDate": req.body.endDate,
-            "name": req.body.name,
-            "quantity": req.body.quantity,
-            "roomId": req.body.roomId,
-            "startDate": req.body.startDate,
-        }
-    ],
+            {
+                "endDate": req.body.endDate,
+                "name": req.body.name,
+                "quantity": req.body.quantity,
+                "roomId": req.body.roomId,
+                "startDate": req.body.startDate,
+            }
+        ],
         "confirm": false,
         "instant": true,
         "name": req.body.name,
-            "propertyId":req.body.propertyId,
+        "propertyId":req.body.propertyId,
     }
     options.body = JSON.stringify(options.body)
     request(options, function (error, response) {
@@ -47,7 +47,58 @@ router.post('/', function(req, res, next) {
                 totalOriginalPrice:response.body.bookings[0].totalOriginalPrice,
                 cartId:response.body.id,
                 dateCreated:response.body.bookings[0].dateCreated,
-                name:response.body.bookings[0].name,
+                name:response.body.bookings[0].name + " - FROM:[" + response.body.bookings[0].bookingRooms[0].startDate + "] TO: [" + response.body.bookings[0].bookingRooms[0].endDate + "]" ,
+                valid:true
+            })
+        }
+        else {
+            res.send({valid:false})
+        }
+    })
+});
+router.post('/confirm', function(req, res, next) {
+    console.log(req.body);
+
+    var db = new JSONdb('./db.json');
+    var token = db.JSON();
+    var options = {
+        'method': 'post',
+        'url': 'https://stage-api.travia.is/api/v1/travelAgents/577/bookingCarts',
+        'headers': {
+            'Authorization': 'Bearer ' + token.access_token,
+            'Content-Type': 'application/json',
+
+        },
+        'body': {}
+    };
+    options.body = {
+
+        "bookingRooms": [
+            {
+                "endDate": req.body.endDate,
+                "name": req.body.name,
+                "quantity": req.body.quantity,
+                "roomId": req.body.roomId,
+                "startDate": req.body.startDate,
+            }
+        ],
+        "confirm": false,
+        "instant": true,
+        "name": req.body.name,
+        "propertyId":req.body.propertyId,
+    }
+    options.body = JSON.stringify(options.body)
+    request(options, function (error, response) {
+        response.body = JSON.parse(response.body)
+        if(response.body.bookingCartStatusCode==="OPEN") {
+            res.send({
+                totalQuantity:response.body.bookings[0].totalQuantity,
+                totalPrice:response.body.bookings[0].totalPrice,
+                totalPax:response.body.bookings[0].totalPax,
+                totalOriginalPrice:response.body.bookings[0].totalOriginalPrice,
+                cartId:response.body.id,
+                dateCreated:response.body.bookings[0].dateCreated,
+                name:response.body.bookings[0].name + " - FROM:[" + response.body.bookings[0].bookingRooms[0].startDate + "] TO: [" + response.body.bookings[0].bookingRooms[0].endDate + "]" ,
                 valid:true
             })
         }
