2e0397acebbe2b8f3667d7e01b2fba1dc42d3dbb
Asdisarson <egill@islandsvefir.is>
2022-10-07 16:10:13 +0000

    m

--- routes/search.js
+++ routes/search.js
@@ -278,22 +278,27 @@ router.get('', function (req, res, next) {
                 data
             );
         }
-if (req.query.city) {
-    var s = req.query.city;
-    var translate = {
-        "á": "a", "ö": "o", "ú": "u",
-        "Á": "A", "Ö": "O", "Ú": "U",
-        "Ý": "Y", "í": "i", "Í": "I",
-        "ý": "y", "ó": "o", "Ó": "O"   // probably more to come
-    };
-    var translate_re = /[áÁöÖúÚóÓýÝíÍ]/g;
-    var str= ( s.replace(translate_re, function(match) {
-        return translate[match];
-    }) );
-    array.result = array.result.filter(hotel => hotel.city.replace(translate_re, function(match) {
-        return translate[match];
-    }) === (req.query.city||str));
-}
+        try {
+            if (req.query.city) {
+                var s = req.query.city;
+                var translate = {
+                    "á": "a", "ö": "o", "ú": "u",
+                    "Á": "A", "Ö": "O", "Ú": "U",
+                    "Ý": "Y", "í": "i", "Í": "I",
+                    "ý": "y", "ó": "o", "Ó": "O"   // probably more to come
+                };
+                var translate_re = /[áÁöÖúÚóÓýÝíÍ]/g;
+                var str= ( s.replace(translate_re, function(match) {
+                    return translate[match];
+                }) );
+                array.result = array.result.filter(hotel => hotel.city.replace(translate_re, function(match) {
+                    return translate[match];
+                }) === (req.query.city||str));
+            }
+        }
+        catch (e) {
+              console.log(e);
+        }
         if (array.result.length === 0) {
             array.result.push({
                 query: JSON.stringify(req.query),
