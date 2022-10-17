af946485a1d09c7782954786084654afd3328d5f
Asdisarson <egill@islandsvefir.is>
2022-10-17 11:31:16 +0000

    m

--- routes/search.js
+++ routes/search.js
@@ -281,9 +281,7 @@ router.get('', function (req, res, next) {
                 }
                 if (response.body[k].rooms[i].images.length > 0) {
                     for (let j = 0; j < response.body[k].rooms[i].images.length; j++) {
-                     var obj = {}
-                        obj["image"] = response.body[k].rooms[i].images[j].filePath
-                        room["gallery" + j] = response.body[k].rooms[i].images[j].filePath
+                     var   obj = response.body[k].rooms[i].images[j].filePath
                         gallery.push(obj);
                     }
                 }
