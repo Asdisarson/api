0098e0bec5501a6c47d8700a453570d702e85300
Asdisarson <egill@islandsvefir.is>
2022-10-17 11:00:48 +0000

    m

--- routes/search.js
+++ routes/search.js
@@ -281,7 +281,7 @@ router.get('', function (req, res, next) {
                 if (response.body[k].rooms[i].images.length > 0) {
                     for (let j = 0; j < response.body[k].rooms[i].images.length; j++) {
                      var obj = {}
-                        obj[i] = response.body[k].rooms[i].images[j].filePath
+                        obj[j] = response.body[k].rooms[i].images[j].filePath
                         gallery.push(obj);
                     }
                 }
