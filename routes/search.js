d81e7c0b6baa19f500cd4886cd67f2c1d398aeab
Asdisarson <egill@islandsvefir.is>
2022-10-17 10:47:04 +0000

    m

--- routes/search.js
+++ routes/search.js
@@ -111,7 +111,7 @@ router.get('', function (req, res, next) {
                 pricesFrom: '',
                 pricesFromCurrencySymbol: '',
                 featureImage: '',
-                gallery: {},
+                gallery: [],
                 latitude: '',
                 longitude: '',
                 hotelId: '',
@@ -219,9 +219,9 @@ router.get('', function (req, res, next) {
 
                 }
                 var temp = response.body[k].images[i].filePath;
-                data.gallery[i] = temp
+                data.gallery.push(temp)
             }
-            data.gallery = data.gallery;
+            data.gallery = data.gallery.reverse();
             for (var i = 0; i < response.body[k].rooms.length; i++) {
                 var room = {
 
@@ -259,13 +259,13 @@ router.get('', function (req, res, next) {
                     extraBedOriginalPrice: parseInt(response.body[k].rooms[i].extraBedOriginalPrice).toLocaleString('de-DE')
                     ,
                     featureImage: "",
-                    gallery: {},
+                    gallery: [],
                     roomAddonCategories: [],
                     booking: '',
                     cancellationPolicyLastDay : ""
                 }
                 var addons = [];
-                var gallery ={} ;
+                var gallery =[] ;
                 if (room.available && req.query.start && req.query.end && req.query.numberOfPeople) {
                     room.booking = "?add-to-cart=1209&propertyId=" + response.body[k].rooms[i].propertyId +
                         "&roomId=" + response.body[k].rooms[i].id + "&product_id=1209" + "&startDate=" +
@@ -279,11 +279,11 @@ router.get('', function (req, res, next) {
                 if (response.body[k].rooms[i].images.length < 0) {
                     for (let j = 0; j < response.body[k].rooms[i].images.length; j++) {
                      var   obj = response.body[k].rooms[i].images[j].filePath
-                        gallery[k] = obj;
+                        gallery.push(obj);
                     }
                 }
 
-                room.gallery = gallery;
+                room.gallery = gallery.reverse();
                 for (let j = 0; j < response.body[k].rooms[i].roomAddonCategories.length; j++) {
                     addons.push({
                         name: response.body[k].rooms[i].roomAddonCategories[j].name,
