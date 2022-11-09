1992763eaa21d530c14b3deab3778809a67e18f4
Asdisarson <egill@islandsvefir.is>
2022-11-09 13:14:02 +0000

    test new searchtewst.js

--- routes/search.js
+++ routes/search.js
@@ -295,7 +295,6 @@ router.get('', function (req, res, next) {
                 }
                 room.addons = addons;
                 var cancellationPolicy = getHotelCache(data.hotelId);
-                cancellationPolicy = cancellationPolicy.information;
                 if(cancellationPolicy) {
                     for (var x = 0; x < cancellationPolicy.information.cancellationPolicy.cancellationPolicyRules.length; x++) {
                         var date = new Date(req.query.start * 1000)
