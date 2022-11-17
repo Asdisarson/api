f8cc49a5c6ad0a9de8db69c20b79ba105d33a8ea
Asdisarson <egill@islandsvefir.is>
2022-11-17 15:05:47 +0000

    woorewards fix

--- routes/niceDollars.js
+++ routes/niceDollars.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+const request1 = require("./request");
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
@@ -8,19 +9,37 @@ router.post('/', function(req, res, next) {
     console.log(req.body)
     if(req.header('api-key-nicebooking') === "nicedollars") {
         var points = 0;
+        e = {
+            isError : true,
+            msg: {
+
+            }
+        }
         var url = "https://nifty-cannon.134-209-30-23.plesk.page/wp-json/woorewards/v1/points/"
         if(req.body.amount) {
             points = (parseInt(req.body.amount)*0.1).toFixed(0);
+            e.msg.amount = req.body.amount;
         }
         var user_email = ""
         if(req.body.email) {
             user_email = req.body.email;
-        }
+            e.msg.email= req.body.email;
 
+        }
+        var order = 0
+        if (req.body.order) {
+            order = "Purchase - Order ID: " + req.body.order;
+            e.msg.order = req.body.order;
+
+        }
+        if(order && user_email && points ) {
+            e.isError = false
+        }
+        if(!e.isError) {
             console.log(points)
-           var options = {
+            var options = {
                 'method': 'PUT',
-                'url': url + user_email + '/nicebooking/-'+ points +'/' + req.body.notes,
+                'url': url + user_email + '/nicebooking/-'+ points +'/' + order,
                 'headers': {
                     'Authorization': 'Basic cmVzdDptcUdDIElvUXYgUFFlSiBkcHBoIGlXRGMgYUFDbQ==',
                     'Content-Type': 'application/json'
@@ -29,36 +48,44 @@ router.post('/', function(req, res, next) {
 
 
             };
-        var  options1 = {
-            'method': 'PUT',
-            'url': url + 'hallo@pineapple.is/nicebooking/-' + points + '/' + req.body.notes,
-            'headers': {
-                'Authorization': 'Basic cmVzdDptcUdDIElvUXYgUFFlSiBkcHBoIGlXRGMgYUFDbQ==',
-                'Content-Type': 'application/json'
-            },
-            body: ''
-        }
-        request(options, function (error, response) {
-            if (error) throw new Error(error);
-            if(response.statusCode >= 400) {
-                res.sendStatus(503)
-                return false;
-            }
-            var request1 = require('request');
 
-            return request1(options1, function (error, response) {
+            var  options1 = {
+                'method': 'PUT',
+                'url': url + 'hallo@pineapple.is/nicebooking/-' + points + '/' + order,
+                'headers': {
+                    'Authorization': 'Basic cmVzdDptcUdDIElvUXYgUFFlSiBkcHBoIGlXRGMgYUFDbQ==',
+                    'Content-Type': 'application/json'
+                },
+                body: ''
+            }
+            request(options, function (error, response) {
                 if (error) throw new Error(error);
                 if(response.statusCode >= 400) {
                     res.sendStatus(503)
                     return false;
-                }else {
-                    res.sendStatus(200)
                 }
-            })
-        })
-1
+                var request1 = require('request');
 
-    }  else {
+                return request1(options1, function (error, response) {
+                    if (error) throw new Error(error);
+                    if(response.statusCode >= 400) {
+                        res.sendStatus(503)
+                        return false;
+                    }else {
+                        res.sendStatus(200)
+                    }
+                })
+            })
+
+
+        }   else {
+            res.send(
+               e
+            )
+
+        }
+        }
+       else {
         res.sendStatus(401)
     }
 });
