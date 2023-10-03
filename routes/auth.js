e6130e3cd7430ca79760aabbc514c0bcfe92ba06
asdisarson <egill@islandsvefir.is>
2023-10-03 13:06:12 +0000

    Update .gitignore and auth token URL
    .gitignore has been significantly expanded to ignore typical files & folders for Node.js projects, resulting in a cleaner repository. Also changed the auth token URL in auth.js to point to the staging API for testing purposes.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
     'method': 'POST',
-    'url': 'https://stage-api.travia.is/oauth/token',
+    'url': 'https://api.travia.is/oauth/token',
     'headers': {
         'Authorization': 'Basic Z29kby1pczpnb2RvQXBwbGljYXRpb24=',
         'Content-Type': 'application/x-www-form-urlencoded'
